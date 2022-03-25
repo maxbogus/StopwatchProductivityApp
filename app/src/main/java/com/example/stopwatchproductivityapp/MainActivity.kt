466428b6d13e404b3915692e37fca9f0d6a819ec
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-03-25 16:30:20 +0300

    Update MainActivity.kt
    - added progress bar.

--- app/src/main/java/com/example/stopwatchproductivityapp/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchproductivityapp/MainActivity.kt
@@ -1,19 +1,34 @@
 package org.hyperskill.stopwatch
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.DrawableCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val handler = Handler(Looper.getMainLooper())
     var timer = 0
     var isTimerRunning = false
+    val colorList: Array<Int> = arrayOf(Color.GRAY,Color.BLACK,Color.BLUE, Color.GREEN, Color.RED)
+    var currentColor = Color.GRAY
     private val updateTimer: Runnable = object : Runnable {
         override fun run() {
             val textView = findViewById<TextView>(R.id.textView)
+            val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+            var result: Int
+            do {
+                val pickedColor = colorList[(colorList.indices).random()]
+                result = pickedColor
+            } while (pickedColor == currentColor)
+            progressBar.indeterminateTintList = ColorStateList.valueOf(result)
             timer += 1
             val minutes = timer / 60
             val seconds = timer % 60
@@ -35,13 +50,18 @@ class MainActivity : AppCompatActivity() {
 
         startButton.setOnClickListener {
             if (!isTimerRunning) {
-                handler.postDelayed(updateTimer, 100)
+                val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                progressBar.indeterminateTintList = ColorStateList.valueOf(currentColor)
+                progressBar.visibility = View.VISIBLE
+                handler.postDelayed(updateTimer, 1000)
                 isTimerRunning = true
             }
         }
         resetButton.setOnClickListener {
             if (isTimerRunning) {
                 handler.removeCallbacks(updateTimer)
+                val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                progressBar.visibility = View.INVISIBLE
                 isTimerRunning = false
                 timer = 0
                 handler.post(finishTimer)
